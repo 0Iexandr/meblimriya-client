58848e3f0d7d7d5f639f3003673d24cc623ce5c7
Oleksandr Kryvoniuk <sasha12887@gmail.com>
2023-11-17 19:10:22 +0200

    added discount info

---
+++ src/components/Discount/Discount.jsx
@@ -0,0 +1,18 @@
+import './Discount.scss';
+
+import ringing from '../../assets/svg/ringing.svg';
+
+const Discount = () => {
+  return (
+    <div className="discount">
+      <img src={ringing} alt="ringing" className="discount__img" />
+      <p className="discount__description">
+        На замовлення{' '}
+        <span className="discount__description-span">від 7000 грн</span>{' '}
+        доставка Безкоштовна!
+      </p>
+    </div>
+  );
+};
+
+export default Discount;
--- src/layout/Layout/Layout.jsx
+++ src/layout/Layout/Layout.jsx
@@ -1,9 +1,12 @@
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 
+import Discount from '../../components/Discount/Discount';
+
 const Layout = ({ children }) => {
   return (
-    <div className="layout">
+    <div>
+      <Discount />
       <Header />
       <main>{children}</main>
       <Footer />
