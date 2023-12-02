628ec5ec38314c978852d9c435810c2cef98aa32
Oleksandr Kryvoniuk <sasha12887@gmail.com>
2023-12-03 00:52:01 +0200

    added white style

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,11 @@
 import './Button.scss';
 
-const Button = ({ text, onClick, arrow }) => {
+const Button = ({ text, onClick, arrow, white }) => {
   return (
-    <button className="button" onClick={onClick}>
+    <button
+      className={`button ${white ? 'button__white' : 'button__brown'}`}
+      onClick={onClick}
+    >
       {text}
       {arrow ? <span className="button__arrow">→</span> : null}
     </button>
