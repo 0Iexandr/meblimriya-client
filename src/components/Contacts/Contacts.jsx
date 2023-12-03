b866b11c8d22b89126a2960b00037f06baba6234
Oleksandr Kryvoniuk <sasha12887@gmail.com>
2023-12-03 23:33:39 +0200

    added icons to numbers

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,5 +1,8 @@
 import './Contacts.scss';
 
+import viber from '../../assets/svg/viber.svg';
+import telegram from '../../assets/svg/telegram.svg';
+
 const Contacts = () => {
   return (
     <div className="contacts">
@@ -7,10 +10,18 @@ const Contacts = () => {
         Контакти для замовленнь та консультації
       </h3>
       <a href="tel:+380970406412" className="contacts__number">
-        +380 97 040 64 12
+        +380 97 040 64 12{' '}
+        <span className="contacts__icons">
+          <img src={viber} alt="viber" />
+          <img src={telegram} alt="telegram" />
+        </span>
       </a>
       <a href="tel:+380980112298" className="contacts__number">
-        +380 98 011 22 98
+        +380 98 011 22 98{' '}
+        <span className="contacts__icons">
+          <img src={viber} alt="viber" />
+          <img src={telegram} alt="telegram" />
+        </span>
       </a>
       <h3 className="contacts__title">Електронна адреса</h3>
       <a href="mailto:mriamebli@gmail.com" className="contacts__mail">
